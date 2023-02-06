86b6272631abbe4bf72b67f8f96049845814cb87
James Wang <jameswang9909@hotmail.com>
2023-02-06 13:14:00 -0800

    Remove logs

--- src/main/kotlin/com/github/james9909/enderswap/EnderSwap.kt
+++ src/main/kotlin/com/github/james9909/enderswap/EnderSwap.kt
@@ -12,18 +12,15 @@ class EnderSwap() : JavaPlugin() {
 
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<String>): Boolean {
         if (sender !is Player) {
-            sender.sendMessage("This command can only be used by a player!")
             return true
         }
 
         if (args.isEmpty() || args.size > 1) {
-            sender.sendMessage("Usage: /enderswap <1-3>")
             return true
         }
 
         val row = args[0].toIntOrNull()
         if (row == null || row < 1 || row > 3) {
-            sender.sendMessage("Invalid row number! Row must be a number between 1 and 3.")
             return true
         }
 
@@ -33,13 +30,11 @@ class EnderSwap() : JavaPlugin() {
 
         for (i in 0..8) {
             val tmp = hotbar.getItem(i)
-            println("exchanging $tmp with ${enderChest.getItem(enderChestStartIndex + i)}")
             hotbar.setItem(i, enderChest.getItem(enderChestStartIndex + i))
             enderChest.setItem(enderChestStartIndex + i, tmp)
         }
 
         sender.updateInventory()
-        sender.sendMessage("Swapped hotbar with row $row in the enderchest!")
         return true
     }
 }
