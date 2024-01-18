66da5656b8b95f260ffff01b3655bccb3dfd4c28
James Wang <jameswang9909@hotmail.com>
2024-01-17 22:49:13 -0800

    Update MagicSpells

--- src/main/java/com/github/james9909/customspells/VariableCastSpell.java
+++ src/main/java/com/github/james9909/customspells/VariableCastSpell.java
@@ -5,19 +5,22 @@ import com.nisovin.magicspells.Subspell;
 import com.nisovin.magicspells.spells.TargetedEntityFromLocationSpell;
 import com.nisovin.magicspells.spells.TargetedEntitySpell;
 import com.nisovin.magicspells.spells.TargetedLocationSpell;
+import com.nisovin.magicspells.util.CastResult;
 import com.nisovin.magicspells.util.MagicConfig;
-import org.bukkit.Location;
-import org.bukkit.entity.LivingEntity;
+import com.nisovin.magicspells.util.SpellData;
+import com.nisovin.magicspells.util.config.ConfigData;
 import org.bukkit.entity.Player;
 
 public class VariableCastSpell extends com.nisovin.magicspells.spells.instant.VariableCastSpell implements TargetedLocationSpell, TargetedEntitySpell, TargetedEntityFromLocationSpell {
+    private final ConfigData<String> variableName = this.getConfigDataString("variable-name", null);
+
     public VariableCastSpell(MagicConfig config, String spellName) {
         super(config, spellName);
     }
 
-    public Subspell getSubSpell(LivingEntity entity) {
-        if (entity instanceof Player player) {
-            String variableName = getVariableName();
+    public Subspell getSubSpell(SpellData data) {
+        if (data.caster() instanceof Player player) {
+            String variableName = this.variableName.get(data);
             if (variableName == null) return null;
             String value = MagicSpells.getVariableManager().getVariable(variableName).getStringValue(player);
 
@@ -31,100 +34,32 @@ public class VariableCastSpell extends com.nisovin.magicspells.spells.instant.Va
         return null;
     }
 
-    @Override
-    public PostCastAction castSpell(LivingEntity caster, SpellCastState state, float power, String[] args) {
-        Subspell s = this.getSubSpell(caster);
+    public CastResult cast(SpellData data) {
+        Subspell s = this.getSubSpell(data);
         if (s != null) {
-            s.subcast(caster, power, args);
+            SpellCastResult result = s.subcast(data);
+            return new CastResult(result.action, result.data);
         }
-        return PostCastAction.HANDLE_NORMALLY;
+        return new CastResult(PostCastAction.HANDLE_NORMALLY, data);
+    }
+
+    public SpellCastResult hardCast(SpellData data) {
+        CastResult result = this.cast(data);
+        return new SpellCastResult(SpellCastState.NORMAL, result.action());
     }
 
     @Override
-    public boolean castAtEntityFromLocation(LivingEntity caster, Location from, LivingEntity target, float power, String[] args) {
-        Subspell s = this.getSubSpell(caster);
-        if (s != null) {
-            return s.subcast(caster, from ,target, power, args);
-        }
-        return true;
+    public CastResult castAtEntityFromLocation(SpellData data) {
+        return this.cast(data);
     }
 
     @Override
-    public boolean castAtEntityFromLocation(Location from, LivingEntity target, float power, String[] args) {
-        return false;
+    public CastResult castAtEntity(SpellData data) {
+        return this.cast(data);
     }
 
     @Override
-    public boolean castAtEntityFromLocation(LivingEntity caster, Location from, LivingEntity target, float power) {
-        Subspell s = this.getSubSpell(caster);
-        if (s != null) {
-            return s.subcast(caster, from, target, power, null);
-        }
-        return true;
-    }
-
-    @Override
-    public boolean castAtEntityFromLocation(Location from, LivingEntity target, float power) {
-        return false;
-    }
-
-    @Override
-    public boolean castAtEntity(LivingEntity caster, LivingEntity target, float power, String[] args) {
-        Subspell s = this.getSubSpell(caster);
-        if (s != null) {
-            return s.subcast(caster, target, power, args);
-        }
-        return true;
-    }
-
-    @Override
-    public boolean castAtEntity(LivingEntity target, float power, String[] args) {
-        return false;
-    }
-
-    @Override
-    public boolean castAtEntity(LivingEntity caster, LivingEntity target, float power) {
-        Subspell s = this.getSubSpell(caster);
-        if (s != null) {
-            return s.subcast(caster, target, power, null);
-        }
-        return true;
-    }
-
-    @Override
-    public boolean castAtEntity(LivingEntity caster, float power) {
-        Subspell s = this.getSubSpell(caster);
-        if (s != null) {
-            return s.subcast(caster, caster.getLocation(), power, null);
-        }
-        return true;
-    }
-
-    @Override
-    public boolean castAtLocation(LivingEntity caster, Location target, float power, String[] args) {
-        Subspell s = this.getSubSpell(caster);
-        if (s != null) {
-            return s.subcast(caster, target, power, args);
-        }
-        return true;
-    }
-
-    @Override
-    public boolean castAtLocation(Location target, float power, String[] args) {
-        return false;
-    }
-
-    @Override
-    public boolean castAtLocation(LivingEntity caster, Location target, float power) {
-        Subspell s = this.getSubSpell(caster);
-        if (s != null) {
-            return s.subcast(caster, target, power, null);
-        }
-        return true;
-    }
-
-    @Override
-    public boolean castAtLocation(Location target, float power) {
-        return false;
+    public CastResult castAtLocation(SpellData data) {
+        return this.cast(data);
     }
 }
